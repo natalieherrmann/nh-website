b544611c3bfa4948d3ac2fdffbd5e806635e679c
Natalie Herrmann <nherrma1@hawk.iit.edu>
2018-12-15 16:33:41 -0600

    Add responsive navigation menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,9 +7,27 @@
 
   // Run JS once DOM is loaded
   document.addEventListener('DOMContentLoaded', function() {
+    var navigation = document.querySelector('#navigation');
+    var nav_bar = document.querySelector('#nav-bar');
+    // nav_heading will be prepended to navigation and will consist of anchor
+    // tag menu_list for opening/closing menu at smaller screen sizes
+    var nav_heading = document.createElement('h2');
+    var menu_list = document.createElement('a');
     // Change 'nojs' class for each html document to 'js'
     document.querySelector('html').className = 'js';
-
+    menu_list.textContent = 'Menu';
+    menu_list.setAttribute('id', 'menu-button');
+    menu_list.setAttribute('href', '');
+    menu_list.classList.add('visible');
+    // menu_list's visible class is shown at smaller screen sizes, and has
+    // display: none at larger screen sizes
+    nav_heading.append(menu_list);
+    navigation.insertBefore(nav_heading, nav_bar);
+    menu_list.addEventListener('click', function(e) {
+      nav_bar.classList.toggle('visible');
+      // Show/hide the menu links when menu is clicked
+      e.preventDefault();
+    });
   });
 
 })();
